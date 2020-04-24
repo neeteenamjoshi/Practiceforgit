776dc63d0b525a275655e7515f45cef45e7c82a2
Neetee Namjoshi <neetee.namjoshi@git.assetvantage.com>
2020-04-24 16:04:20 +0530

    code for build

--- important queries/dump.sql
+++
@@ -1,9 +0,0 @@
-SELECT *  FROM `companymaster` WHERE `created` LIKE '%2019-06-14%'
-UPDATE `companymaster` SET `yearend` = '201412'  WHERE `created` LIKE '%2019-07-02%';
-
-UPDATE `companymaster` SET`avisin`= concat('AV',`isin`) WHERE `symbol` IN ('USDY','OUSA','OGIG','OEUR') and `created` LIKE '%2019-05-27%'
-UPDATE `companymaster` SET `avisin`= concat('AV',`isin`) WHERE `created` LIKE '%2019-06-14%' and `exchange` = 54
-
-SELECT null, `fincode`, `avisin`, `companyname`, `shortname`, `uniqueid`, `industrycode`, `industryname`, `housecode`, `housename`, `scripcode`, `scripgroup`, `symbol`, `series`, `isin`, `schemetype`, `exchange`, `yearend`, `price`, `marketcap`, `facevalue`, `countryid`, `isunlisted`, `productkey`, `oldvaluation`, `created`, `updated`, `createdby`, `updatedby` FROM `companymaster` WHERE `created` LIKE '%2019-07-02%' and exchange =52
-
-
--- important queries/positionmaster-1.sql
+++
@@ -1,3 +0,0 @@
-SET @disable_triggers := 1;
-update positionmaster p,masters.companymaster c set p.instrumentname = c.companyname where p.instrumentid = c.id and p.vehicleid =1;
-SET @disable_triggers := 0;
